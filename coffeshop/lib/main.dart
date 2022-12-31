122a2085e8e3c4ee6ff5c7bdcbe6b2772843b4d2
Roberth-win <rjromerol1999@gmail.com>
2022-12-30 19:09:08 -0500

    Buttons and Icons

--- coffeshop/lib/main.dart
+++ coffeshop/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: UserLogin(),
+    home: Login(),
   ));
 }
 
@@ -14,26 +14,38 @@ class UserLogin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.indigo[900], //#16335B,
-        title: Text("Coffe App"),
+        body: //Column(
+            // children: <Widget>[
+            Center(
+      // child: Image.asset("assets/logo.png"),
+      child: Icon(
+        Icons.add_shopping_cart,
+        color: Colors.indigoAccent[400],
       ),
+    )
+        // ],
+        );
+    // );
+  }
+}
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       body: Center(
-        child: Text(
-          "Scan QR Code",
-          style: TextStyle(
-            fontSize: 24,
-            letterSpacing: 2.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.red,
-            fontFamily: 'Raleway',
+        child: ElevatedButton.icon(
+          icon: Icon(Icons.account_circle),
+          label: Text("Login"),
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.indigo[400],
           ),
+          // child: Text("Login"),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => print("clikceck"),
-        backgroundColor: Colors.indigoAccent,
-        child: Text("+"),
+        // child: Image.asset('assets/logo.png'),
       ),
     );
   }
