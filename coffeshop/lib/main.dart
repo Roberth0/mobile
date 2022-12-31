faa8705cba925205a631aba72ac3920f026a41f4
Roberth-win <rjromerol1999@gmail.com>
2022-12-31 12:08:38 -0500

    statefull widget

--- coffeshop/lib/main.dart
+++ coffeshop/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Login(),
+    home: DataWidget(),
   ));
 }
 
@@ -14,20 +14,29 @@ class UserLogin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: //Column(
-            // children: <Widget>[
-            Center(
-      // child: Image.asset("assets/logo.png"),
-      child: Icon(
-        Icons.add_shopping_cart,
-        color: Colors.indigoAccent[400],
+      body: Padding(
+        padding: EdgeInsets.all(30),
+        child: Column(
+          children: <Widget>[
+            Text("Login"),
+            Expanded(child: Image.network("https://images.unsplash.com/photo-1554600740-951beab4712b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80")),
+            Form(child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                TextFormField(),
+                TextFormField(),
+                SizedBox(height: 20,),
+                ElevatedButton(onPressed: () {}, child: Text("Login")),
+              ],
+            )),
+          ],
+        ),
       ),
-    )
-        // ],
-        );
-    // );
+    );
   }
 }
+
+
 
 class Login extends StatelessWidget {
   const Login({super.key});
@@ -35,18 +44,51 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton.icon(
-          icon: Icon(Icons.account_circle),
-          label: Text("Login"),
-          onPressed: () {},
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.indigo[400],
-          ),
-          // child: Text("Login"),
+        body: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Text("Bienvenido a,"),
+        Image.asset(
+          "assets/logo.png",
+          fit: BoxFit.cover,
         ),
-        // child: Image.asset('assets/logo.png'),
+        ElevatedButton(onPressed: () {}, child: Text("Login")),
+        OutlinedButton(onPressed: () {}, child: Text("Sign Up"))
+      ],
+    ));
+  }
+}
+
+class DataWidget extends StatefulWidget {
+  const DataWidget({super.key});
+
+  @override
+  State<DataWidget> createState() => _DataWidgetState();
+}
+
+class _DataWidgetState extends State<DataWidget> {
+  int counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Coffe Shop"),
       ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("$counter", style: TextStyle(fontSize: 32, color: Colors.indigo[800])),
+          ]
+        ),
+      ),
+        floatingActionButton: FloatingActionButton(onPressed: (){
+          setState(() {
+            counter += 1;
+          });
+        },
+          child: Icon(Icons.add),
+        ),
     );
   }
 }
